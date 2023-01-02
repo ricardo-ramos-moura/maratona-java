ce02c7d43d6ecb717041a6df32456f0137bb717b
Ricardo Ramos <ricardo.moura@teltelecom.com.br>
2023-01-02 18:06:26 -0300

    class 09 - comments

---
+++ maratona-java-devdojo/src/academy/devdojo/comentarios/Comentarios.java
@@ -0,0 +1,19 @@
+package academy.devdojo.comentarios;
+
+public class Comentarios {
+    /**
+        Isso é um comentário <b>javadoc</>
+     */
+    public static void main(String[] args) {
+        //Isso é um comentário de uma linha
+
+        /*
+            Isso é um comentário
+            de múltiplas
+            linhas
+         */
+
+        System.out.println("HA-ME-HA-ME-HAAA");
+
+    }
+}
