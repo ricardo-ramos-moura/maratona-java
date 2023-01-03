269d9283183fea45a8d9dbb7fbc50739e70cd961
Ricardo Ramos <ricardo.moura@teltelecom.com.br>
2023-01-03 08:47:12 -0300

    class 11 - Primitive types - declaration and size in memory

--- maratona-java-devdojo/src/academy/devdojo/introducao/TiposPrimitivos.java
+++ maratona-java-devdojo/src/academy/devdojo/introducao/TiposPrimitivos.java
@@ -4,7 +4,17 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long bigNumber = 100000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = -128;
+        short ageShort = 10;
+        boolean trueBoolean = true;
+        boolean falseBoolean = false;
+        char character = 'R';
         System.out.println("Age is " + age + " years");
+        System.out.println(falseBoolean);
+        System.out.println("Char: " + character);
 
     }
 }
