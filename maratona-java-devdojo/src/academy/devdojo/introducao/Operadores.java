7268690639353ccab17cba3a5a1e6ac3fa343461
Ricardo Ramos <ricardo.moura@teltelecom.com.br>
2023-01-03 09:24:33 -0300

    class 15 - arithmetic operators

---
+++ maratona-java-devdojo/src/academy/devdojo/introducao/Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.introducao;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 10;
+
+        double result = number01 / number02;
+
+        System.out.println(result);
+    }
+}
