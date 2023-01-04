abfeee06079699b482493c731fab2ecfb3684e4a
Ricardo Ramos <ricardo.moura@teltelecom.com.br>
2023-01-04 09:17:14 -0300

    class 16 - relational operators

--- maratona-java-devdojo/src/academy/devdojo/introducao/Operadores.java
+++ maratona-java-devdojo/src/academy/devdojo/introducao/Operadores.java
@@ -6,8 +6,27 @@ public class Operadores {
         int number01 = 10;
         int number02 = 10;
 
-        double result = number01 / number02;
-
+        double result = number01 / (double) number02;
         System.out.println(result);
+
+
+        // % rest
+        int rest = 20 % 10;
+        System.out.println(rest);
+
+        // > < >= <= !=
+        boolean isTenLargerTwenty = 10 > 20;
+        boolean isTenSmallerTwenty = 10 < 20;
+        boolean isTenLargerEqualTwenty = 10 >= 20;
+        boolean isTenSmallerEqualTwenty = 10 <= 20;
+        boolean isTenDifferentTwenty = 10 != 20;
+        System.out.println("isTenLargerTwenty " + isTenLargerTwenty);
+        System.out.println("isTenSmallerTwenty " + isTenSmallerTwenty);
+        System.out.println("isTenLargerEqualTwenty " + isTenLargerEqualTwenty);
+        System.out.println("isTenSmallerEqualTwenty " + isTenSmallerEqualTwenty);
+        System.out.println("isTenDifferentTwenty " + isTenDifferentTwenty);
+
+
+
     }
 }
