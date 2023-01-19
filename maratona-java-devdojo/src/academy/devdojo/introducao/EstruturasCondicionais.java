b6ce1469dc507d5a1d2e952a2b9bec30433d2b5c
Ricardo Ramos <ricardo.moura@teltelecom.com.br>
2023-01-19 11:20:43 -0300

    Class 21 - Conditional Scructure - ELSE IF

--- maratona-java-devdojo/src/academy/devdojo/introducao/EstruturasCondicionais.java
+++ maratona-java-devdojo/src/academy/devdojo/introducao/EstruturasCondicionais.java
@@ -7,6 +7,8 @@ public class EstruturasCondicionais {
 
         if (isAuthorizedBuyAlcoholicDrink){
             System.out.println("Autorizado a comprar Bebida Alcólica.");
+        }else{
+            System.out.println("Não autorizado a comprar Bebida Alcólica.");
         }
 
         if (!isAuthorizedBuyAlcoholicDrink){
@@ -17,6 +19,20 @@ public class EstruturasCondicionais {
         if (aux == true){
             System.out.println("Dentro e algo que nunca deve ser feito");
         }
+
+        //Idade < 15 categoria infantil
+        //Idade >= 15 && idade < 18 categoria juvenil
+        //Idade >= 18 && idade categoria adulto
+        age = 20;
+        String category;
+        if (age < 15){
+            category = "Categoria Infantil";
+        }else if (age >= 15 && age < 18){
+            category = "Categoria Juvenil";
+        }else{
+            category = "Categoria Adulto";
+        }
+        System.out.println(category);
 
 
     }
